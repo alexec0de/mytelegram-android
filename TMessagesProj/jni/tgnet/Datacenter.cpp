976b515f9dd38cf6c02093ae25893e5d8d02a383
alexec0de <102225090+alexec0de@users.noreply.github.com>
2026-06-11 17:34:33 +0300

    Update Datacenter.cpp

--- TMessagesProj/jni/tgnet/Datacenter.cpp
+++ TMessagesProj/jni/tgnet/Datacenter.cpp
@@ -1505,12 +1505,12 @@ TL_help_configSimple *Datacenter::decodeSimpleConfig(NativeByteBuffer *buffer) {
 
     static std::string public_key =
             "-----BEGIN RSA PUBLIC KEY-----\n"
-                                                     "MIIBCgKCAQEAu+3tvscWDAlEvVylTeMr5FpU2AjgqzoQHPjzp69r0YAtq0a8rX0M\n"
-                                                     "Ue78F/FRAqBaEbZW6WBzF3AjOlNYpOtvvwGhl9rGCgziunbd9nwcKJBMDWS9O7Mz\n"
-                                                     "/8xjz/swIB4V56XcjOhrjUHJ/GniFKoum00xeEcYnr5xnLesvpVMq97Ga6b+xt3H\n"
-                                                     "RftHY/Zy1dG5zs8upuiAOlEiKilhu1IthfMjFG3NF6TiGrO9YU3YixFbJy67jtHk\n"
-                                                     "v5FarscM2fC5iWQ2eP1y6jXR64sGU3QjncvozYOePrH9jGcnmzUmj42x/H28IjJQ\n"
-                                                     "9EjEc22sPOuauK0IF2QiCGh+TfsKCK189wIDAQAB\n"
+                                                     "MIIBCgKCAQEAhUCF/p27uylgaSUNZk+jvjNhhUEv0wZDodxOVr7uiXJpHl6VLMAX\n"
+                                                     "KchWQLSYsI8Ni3nu4Kscuwlv5QedCWOMXK6j8uw6URwJu5+9iJikc2NjfFUdJ0Xh\n"
+                                                     "Xlx7ZLtxFwwwBAIVck3wIGZg6ntE3e6n/JNRfNS83m6SPL6VQxxurS5mPJVlR1+I\n"
+                                                     "WQNxGiSUyIBA/LHhGsDisQwe4CX9r8UDn7ZnZtgG0GGnd6hliTBil8ixs4sAl+wc\n"
+                                                     "ICFde8RiA4wGY6HsfEL94DUDOOkYLrhoYJJKifFCtxDXvkzEqF5M7kmsZG2Gy3cz\n"
+                                                     "r8syNPw+4W23uM398eHKhPWOqnubZMndeQIDAQAB\n"
                                                      "-----END RSA PUBLIC KEY-----"
                                                      ;
 
